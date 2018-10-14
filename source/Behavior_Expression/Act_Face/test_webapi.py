4f2361f10913403c4147420865dab459d459574a
jschoi <cjs0818@ust.ac.kr>
2018-10-14 12:00:05 +0900

    test_webapi.py added

---
+++ source/Behavior_Expression/Act_Face/test_webapi.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+# -----------------------
+# Web_API class for web POST
+from Interface.Web.post import Web_API
+
+def main():
+    # -----------------------
+    # Web_API class for web POST
+    web_api = Web_API()
+
+    url = 'http://192.168.25.7:60000/message'
+    message = "안녕하세요."
+    message = message + message + message + message + message
+    ani_parameter = {
+            'video_path': "./",
+            'pause': 0,
+            'audio_enable': 0,
+            'video_delay': 100,
+            'audio_length': len(message)
+        }
+    data_send = {
+            'speech': message,
+            'param': ani_parameter,
+        }
+
+    web_api.send_post(data_send, url)
+
+#----------------------------------------------------
+# 메인 함수
+#----------------------------------------------------
+if __name__ == '__main__':
+
+    main()
