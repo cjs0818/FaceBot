6fae480a80f2e58fd1762da61324eeda9e66f998
jschoi <cjs0818@ust.ac.kr>
2018-10-21 10:38:45 +0900

    start_docker_compose.sh added

---
+++ source/docker/start_docker_compose.sh
@@ -0,0 +1,13 @@
+export FACEBOT_HOME=~/work/ChatBot/FaceBot/source
+
+#-----------------------
+# 2in1
+#python3 $FACEBOT_HOME/Behavior_Expression/Act_Face/main_server.py &
+#docker-compose -f docker-compose-2in1.yml up
+
+#-----------------------
+# 3in1
+docker-compose -f docker-compose-3in1.yml up
+
+
+#sudo chown -R $USER ~/work/ChatBot/FaceBot/source/Knowledge/Database/MongoDB
