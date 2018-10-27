cd892796d336f3dad713ddae1f89949f7792b91a
jschoi <cjs0818@ust.ac.kr>
2018-10-27 23:19:19 +0900

    test_gstreamer_receive.sh modified

--- source/Behavior_Perception/Percept_Face/test_gstreamer_receive.sh
+++ source/Behavior_Perception/Percept_Face/test_gstreamer_receive.sh
@@ -1,6 +1,6 @@
 # Install gstremaer
 #   Linux
-#	sudo apt-get install gstreamer1.0 gstreamer1.0-tools
+#	sudo apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
 #   OSX
 #	brew install gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
 
